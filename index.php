550a0ac236691cdad0b38ecb51fe49ae0e91e343
Enriko Todorov <impossiblelimner@gmail.com>
2021-02-20 18:34:25 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,69 +1,52 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-$ServerIP = $_GET['ip']; // Взимаме IP-to
-$game = $_GET['game']; // Взимаме играта
+header("Content-type: image/png; charset=utf-8");
 
-// Функция за съкращаване на текста
-function truncate_string($string, $max_chars, $end_chars) {
-    $text_len = strlen($string);
-    $temp_text = '';
-    if ($text_len > $max_chars) {
-        for ($i = 0;$i < $max_chars;$i++) {
-            $temp_text.= $string[$i];
-        }
-        $temp_text.= $end_chars;
-        return $temp_text;
-    } else {
-        return $string;
-    }
+require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/includes/functions.php";
+
+$URL = explode("/", htmlspecialchars($_SERVER['QUERY_STRING']));
+
+if(!checkIP($URL[1])) {
+    header("Location: ../index.ph");
 }
 
-// Взимаме информация от GameQ
 $GameQ = new \GameQ\GameQ();
-$GameQ->addServer(['type' => '' . $game . '', 'host' => '' . $ServerIP . '', ]);
+$GameQ->addServer([
+    'type' => $URL[0],
+    'host' => $URL[1],
+]);
 $GameQ->setOption('timeout', 5);
 $GameQ->addFilter('normalize');
 
 $results = $GameQ->process();
 
-$gameIco = imagecreatefrompng('images/games/' . $game . '.png'); // Генерираме иконката на избраната игра
-$getBack = imagecreatefrompng("images/small.png"); // Генерираме задния фон
+$result = $results[$URL[1]];
 
-foreach ($results as $get) {
+$getBack = imagecreatefrompng("images/small.png");
 
-    $servName = $get["gq_hostname"]; // Взимаме името на сървъра
-
-    if (!$servName) { 
-        $status = imagecreatefrompng('images/offline.png'); // Генерираме иконата, че offline
-        $color = ImageColorAllocate($getBack, 255, 255, 255); // Бял цвят
-
-        imagettftext($getBack, 9, 0, 60, 20, $color, __DIR__ . "/arial.ttf", "$ServerIP в момента не работи!"); // Показваме, че сървъра не работи
-
-        imagecopy($getBack, $status, 4, 10, 0, 0, 9, 14); // Поставяме иконката за offline
-    } else {
-        $players = $get["gq_numplayers"]; // Взимаме играчите
-        $maxplayers = $get["gq_maxplayers"]; // Взимаме общите слотове
-        $servName = $get["gq_hostname"]; // Взимаме името на сървъра
-        $map = $get["gq_mapname"]; // Взимаме името на картата
- 
-        $status = imagecreatefrompng('images/online.png'); // Генерираме иконка за online
-
-        // Цветове, които ще използваме
-        $white = ImageColorAllocate($getBack, 248, 248, 255);
-        $blue = ImageColorAllocate($getBack, 4, 226, 233);
-        $green = ImageColorAllocate($getBack, 0, 209, 0);
-        $orange = ImageColorAllocate($getBack, 244, 164, 0);
-
-        imagettftext($getBack, 7.5, 0, 17, 12, $white, __DIR__ . "/arial.ttf", "" . truncate_string($servName, 49, '..') . ""); // Показваме името на сървъра
-        imagettftext($getBack, 7.5, 0, 17, 25, $blue, __DIR__ . "/arial.ttf", "IP: $ServerIP"); // Показваме IP  адреса
-        imagettftext($getBack, 7.5, 0, 155, 25, $green, __DIR__ . "/arial.ttf", "Играчи: " . $players . "/" . $maxplayers . ""); // Показваме общите играчи
-        imagettftext($getBack, 7.5, 0, 240, 25, $orange, __DIR__ . "/arial.ttf", "Карта: " . truncate_string($map, 10, '..') . ""); // Показваме името на картата
-        imagecopy($getBack, $status, 4, 10, 0, 0, 9, 14); // Добавяме иконката за online
-        imagecopy($getBack, $gameIco, 333, 7, 0, 0, 16, 16); // Добавяме иконката на играта
-    }
+if (file_exists(__DIR__ . "/images/games/$URL[0].png")) {
+    $gameIco = imagecreatefrompng('images/games/' . $URL[0] . '.png');
+    imagecopy($getBack, $gameIco, 333, 7, 0, 0, 16, 16);
 }
-header("Content-type: image/png; charset=utf-8");
+
+$white = ImageColorAllocate($getBack, 248, 248, 255);
+
+if ($result['gq_online'] == '1') {
+    $status = imagecreatefrompng('images/online.png');
+} else {
+    $status = imagecreatefrompng('images/offline.png');
+}
+
+$hostName = (isset($result['gq_hostname']) && $result['gq_hostname'] != "" ? $result['gq_hostname'] : 'N/A');
+$maxPlayers = (isset($result['gq_maxplayers']) && $result['gq_maxplayers'] != "" ? $result['gq_maxplayers'] : '0');
+$onlinePlayers = (isset($result['gq_numplayers']) && $result['gq_numplayers'] != "" ? $result['gq_numplayers'] : '0');
+$map = (isset($result['gq_mapname']) && $result['gq_mapname'] != "" ? $result['gq_mapname'] : 'N/A');
+
+imagettftext($getBack, 7.5, 0, 17, 12, $white, __DIR__ . "/arial.ttf", "" . truncate_string($hostName, 49, '..') . "");
+imagettftext($getBack, 7.5, 0, 155, 25, $white, __DIR__ . "/arial.ttf", "Играчи: $onlinePlayers / $maxPlayers");
+imagettftext($getBack, 7.5, 0, 240, 25, $white, __DIR__ . "/arial.ttf", "Карта: " . truncate_string($map, 8, '..') . "");
+imagettftext($getBack, 7.5, 0, 17, 25, $white, __DIR__ . "/arial.ttf", "IP: $URL[1]");
+imagecopy($getBack, $status, 4, 10, 0, 0, 9, 14);
 
 imagepng($getBack);
 imagedestroy($getBack);
-
